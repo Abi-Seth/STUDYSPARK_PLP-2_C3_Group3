67b07f4916583758c21bedd368b18f791b143113
GLOR205 <g.muhorakey@alustudent.com>
2025-03-23 12:49:52 -0700

    updates

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 class User:
     """This is the users class"""
+    def __init__(self):
+        self.users = {}
+        self.logged_in_user = None
+
+    def register(self, username, password):
+        if username in self.users:
+            print("Username already exists. Please choose a different one.")
+        else:
+            self.users[username] = password
+            print(f"User '{username}' registered successfully!")
+
+    def login(self, username, password):
+        if username not in self.users:
+            print("User not found. Please register first.")
+        elif self.users[username] != password:
+            print("Incorrect password. Please try again.")
+        else:
+            self.logged_in_user = username
+            print(f"User '{username}' logged in successfully!")
+
+    def logout(self):
+        if self.logged_in_user:
+            print(f"User '{self.logged_in_user}' logged out successfully!")
+            self.logged_in_user = None
+        else:
+            print("No user is currently logged in.")
+
+    def is_logged_in(self):
+        return self.logged_in_user is not None
 
 class StudySession:
     """This is the study session class"""
